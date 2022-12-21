73737ac6e47cf73b3ffceccbb37a61d2237c8e48
aspenne <axel.mch@outlook.fr>
2022-12-21 01:31:10 +0100

    add the onclick nav menu + change image hover in the about

--- script.js
+++ script.js
@@ -1,69 +1,4 @@
-/* Don't no why but the web page is crashing
-tab_letters = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r",
-                     "s", "t", "u", "v", "w", "x", "y", "z", "1", "2", "3", "4", "5", "6", "7", "8", "9", "0", 
-                     "!", "@", "#", "$", "%", "&", "?"];
-
-tab_number = [1, 2, 3, 4, 5, 6, 7, 8, 9, 0];
-
-const matrice = document.querySelector(".matrice");
-
-
-for (var i = 0; i < 40; i++) {
-    const letter = document.createElement("div");
-    letter.innerHTML = "a";
-
-    matrice.appendChild(letter);
-
-    function change_char(){
-        const x = parseInt(`${Math.floor(Math.random() * tab_letters.length)}`);
-        letter.innerHTML = tab_letters[x];
-    }
-
-    setInterval(change_char, 100);
-}
-
-function matrixEffect() {
-
-    random_nb = Math.floor(Math.random() * 9) + 1; 
-
-    while(tab_left.includes(random_nb)){
-        random_nb = Math.floor(Math.random() * 9) + 1;
-    }
-
-    tab_left.push(random_nb);
-
-
-    const matrix = document.createElement("div");
-    matrix.classList.add("matrice");
-
-    matrix.style.left = random_nb * 10 + "%";
-
-    setInterval(() => {
-        matrix.innerHTML = matrice.innerHTML;
-    }, 100);
-
-    document.querySelector("body").insertBefore(matrix, matrice);
-
-}
-
-function destroyMatrice() {
-    const matrice = document.querySelectorAll(".matrice");
-    for (var i = 1; i < matrice.length; i++) {
-        matrice[i].remove();
-    }
-}   
-
-tab_left = [];
-
-setInterval(matrixEffect, 800);
-
-setInterval(destroyMatrice, 800*9);
-
-*/
-
-
 /***** Typed *********/
-
 const typed = new Typed(".typed", {
     strings: ["I'm Developper", "I love anime", "I love anime / manga", " I love anime / manga particulary evangilon", " I like to code", 
               "I like winter",  "I like winter, snow", "I like winter, snow, skiing", "I like winter, snow, skiing, landscapes "],
@@ -75,7 +10,7 @@ const typed = new Typed(".typed", {
     backDelay: 1500,
     loop: true,
     showCursor: true,
-    cursorChar: "_",
+    cursorChar: "|",
     contentType: "html",
 });
 
@@ -125,10 +60,9 @@ function white() {
 function deleteWhite() {
     const white = document.querySelectorAll(".white");
 
-    for (var i = 1; i < 10; i++) {
+    for (var i = 0; i < 10; i++) {
         white[i].remove();
     }
-    console.log("cleanning");
 }
 
 // create 10 points every second
@@ -138,3 +72,129 @@ setInterval(white, 100);
 setTimeout(() => {
     setInterval(deleteWhite, 1000);
 }, 2000);
+
+/****** button link ******/
+
+const button1 = document.querySelector(".button1");
+const button2 = document.querySelector(".button2");
+const button3 = document.querySelector(".button3");
+const button4 = document.querySelector(".button4");
+
+button1.addEventListener("click", () => {
+    location.href = "#about";
+});
+
+button2.addEventListener("click", () => {
+    location.href = "#projects";
+});
+
+button3.addEventListener("click", () => {
+    location.href = "#contact";
+});
+
+button4.addEventListener("click", () => {
+    location.href = "#contact";
+});
+
+/*********** about me image apparition *************/
+
+// get all the images to show
+const louannec = document.querySelector(".louannec");
+const computer_science = document.querySelector(".computer_science");
+const github_copilot = document.querySelector(".github_copilot");
+const chat_gpt = document.querySelector(".chat_gpt");
+const dall_e = document.querySelector(".dall_e");
+
+// the image and legend to change 
+const image = document.querySelector(".img_text");
+const legend = document.querySelector(".to_type");
+
+var finished = true;
+
+
+louannec.addEventListener("mouseover", () => {
+    if (finished){
+        finished = false;
+        image.style.animation = "disapparing 1s";
+        legend.style.animation = "disapparing 1s";
+
+        setTimeout(() => {
+            image.src = "images/europe.jpg";
+            legend.innerHTML = "Europe";
+            legend.style.animation = "apparing 1s";
+            image.style.animation = "apparing 1s";
+            setTimeout(() => {
+                image.style.animation = "disapparing 1s";
+                legend.style.animation = "disapparing 1s";
+            }, 1000);
+            setTimeout(() => {
+                image.style.animation = "apparing 1s";
+                legend.style.animation = "apparing 1s";
+                image.src = "images/brittany.jpg";
+                legend.innerHTML = "Brittany";
+            }, 2000);
+            setTimeout(() => {
+                image.style.animation = "disapparing 1s";
+                legend.style.animation = "disapparing 1s";
+            }, 3000);
+            setTimeout(() => {
+                image.style.animation = "apparing 1s";
+                legend.style.animation = "apparing 1s";
+                image.src = "images/louannec.jpg";
+                legend.innerHTML = "Louannec";
+            }, 4000);
+            setTimeout(() => {
+                finished = true;
+            }, 5000);
+        }, 1000);
+    }
+});
+
+github_copilot.addEventListener("mouseover", () => {
+    if (finished){
+        finished = false;
+        image.style.animation = "disapparing 1s";
+        legend.style.animation = "disapparing 1s";
+    
+        setTimeout(() => {
+            image.src = "images/github_copilot.png";
+            legend.innerHTML = "Github Copilot";
+            legend.style.animation = "apparing 1s";
+            image.style.animation = "apparing 1s";
+            finished = true;
+        }, 1000);
+    }
+});
+
+chat_gpt.addEventListener("mouseover", () => {
+    if (finished) {
+        finished = false;
+        image.style.animation = "disapparing 1s";
+        legend.style.animation = "disapparing 1s";
+
+        setTimeout(() => {
+            image.src = "images/chat_gpt.jpg";
+            legend.innerHTML = "Open IA / Chat GPT";
+            legend.style.animation = "apparing 1s";
+            image.style.animation = "apparing 1s";
+            finished = true;
+        }, 1000);
+    }
+});
+
+dall_e.addEventListener("mouseover", () => {
+    if (finished) {
+        finished = false;
+        image.style.animation = "disapparing 1s";
+        legend.style.animation = "disapparing 1s";
+
+        setTimeout(() => {
+            image.src = "images/dall_e.jpg";
+            legend.innerHTML = "Open IA / Dall-E";
+            legend.style.animation = "apparing 1s";
+            image.style.animation = "apparing 1s";
+            finished = true;
+        }, 1000);
+    }
+});
+
