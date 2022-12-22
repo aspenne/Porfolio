eda872d3e2f14a2825062fe8cde5b5d6f7c6e269
aspenne <axel.mch@outlook.fr>
2022-12-22 01:21:15 +0100

    scroll effect added / sliding project added / working on : change img, need to make 2 function

--- script.js
+++ script.js
@@ -60,7 +60,7 @@ function white() {
 function deleteWhite() {
     const white = document.querySelectorAll(".white");
 
-    for (var i = 0; i < 10; i++) {
+    for (var i = 0; i < 9; i++) {
         white[i].remove();
     }
 }
@@ -89,7 +89,7 @@ button2.addEventListener("click", () => {
 });
 
 button3.addEventListener("click", () => {
-    location.href = "#contact";
+    location.href = "#cv";
 });
 
 button4.addEventListener("click", () => {
@@ -100,10 +100,11 @@ button4.addEventListener("click", () => {
 
 // get all the images to show
 const louannec = document.querySelector(".louannec");
-const computer_science = document.querySelector(".computer_science");
+const nets = document.querySelector(".nets");
 const github_copilot = document.querySelector(".github_copilot");
 const chat_gpt = document.querySelector(".chat_gpt");
 const dall_e = document.querySelector(".dall_e");
+const evangelion = document.querySelector(".evangelion");
 
 // the image and legend to change 
 const image = document.querySelector(".img_text");
@@ -197,4 +198,168 @@ dall_e.addEventListener("mouseover", () => {
         }, 1000);
     }
 });
+
+nets.addEventListener("mouseover", () => {
+    if (finished) {
+        finished = false;
+        image.style.animation = "disapparing 1s";
+        legend.style.animation = "disapparing 1s";
+
+        setTimeout(() => {
+            image.src = "images/nets.jpg";
+            legend.innerHTML = "Nets";
+            legend.style.animation = "apparing 1s";
+            image.style.animation = "apparing 1s";
+            finished = true;
+        }, 1000);
+    }
+});
+
+evangelion.addEventListener("mouseover", () => {
+    if (finished) {
+        finished = false;
+        image.style.animation = "disapparing 1s";
+        legend.style.animation = "disapparing 1s";
+
+        setTimeout(() => {
+            image.src = "images/evangelion.jpg";
+            legend.innerHTML = "Evangilon";
+            legend.style.animation = "apparing 1s";
+            image.style.animation = "apparing 1s";
+            finished = true;
+        }, 1000);
+    }
+});
+
+/************** Slide project *****************/
+
+/************** Slide to left project *****************/
+
+const sliding_left = document.querySelector(".slide_left");
+
+window.addEventListener("scroll", () => {
+
+    // distance from the top of the page to the top of the viewport and the height of the viewport
+    const {scrollTop, clientHeight} = document.documentElement;
+
+    // distance from the top of the page to the top of the element
+    const topElementToTopviewport = sliding_left.getBoundingClientRect().top;
+
+    // if the distance from the top of the page to the top of the element is less than the distance from the top 
+    // of the page to the top of the viewport + the height of the viewport
+    // the element animation is activated
+    // toFixed() is used to round the number
+
+    if (scrollTop > ( scrollTop + topElementToTopviewport).toFixed() - clientHeight * 0.80) {
+        if (sliding_left.classList.contains("reverse_left")) {
+            sliding_left.classList.remove("reverse_left");
+        }
+        sliding_left.classList.add("left_apparition");
+    }
+
+    // if we already scroll enough to activate the animation and we scroll back we remove the animation left_apparition and add the animation reverse_left
+    if ( (sliding_left.classList.contains("left_apparition")) && (scrollTop < (( scrollTop + topElementToTopviewport).toFixed() - clientHeight * 0.80)) ) {
+        sliding_left.classList.remove("left_apparition");
+        sliding_left.classList.add("reverse_left");
+    }
+});
+
+/************** Slide to right project *****************/
+
+const sliding_right= document.querySelector(".slide_right");
+
+window.addEventListener("scroll", () => {
+
+    // distance from the top of the page to the top of the viewport and the height of the viewport
+    const {scrollTop, clientHeight} = document.documentElement;
+
+    // distance from the top of the page to the top of the element
+    const topElementToTopviewport = sliding_right.getBoundingClientRect().top;
+
+    // if the distance from the top of the page to the top of the element is less than the distance from the top 
+    // of the page to the top of the viewport + the height of the viewport
+    // the element animation is activated
+    // toFixed() is used to round the number
+
+    if (scrollTop > ( scrollTop + topElementToTopviewport).toFixed() - clientHeight * 0.80) {
+        if (sliding_right.classList.contains("reverse_right")) {
+            sliding_right.classList.remove("reverse_right");
+        }
+        sliding_right.classList.add("right_apparition");
+    }
+
+    // if we already scroll enough to activate the animation and we scroll back we remove the animation right_apparition and add the animation reverse_right
+    if ( (sliding_right.classList.contains("right_apparition")) && (scrollTop < (( scrollTop + topElementToTopviewport).toFixed() - clientHeight * 0.80)) ) {
+            sliding_right.classList.remove("right_apparition");
+            sliding_right.classList.add("reverse_right");
+    }
+});
+
+/************** changing image project *****************/
+
+const image_to_switch = document.querySelector(".carrousel_p1");
+const image_project1 = ["images/evangelion.jpg", "images/nets.jpg", "images/chat_gpt.jpg", "images/europe.jpg", "images/profil_pic.jpeg"];
+const btn_prev1 = document.querySelector(".prev1");
+const btn_next1 = document.querySelector(".next1");
+var iteration_img1 = 0;
+
+btn_next1.addEventListener("click", () => {
+    iteration_img1++;
+    var index_img = image_project1[iteration_img1 % image_project1.length];
+
+    setTimeout(() => {
+        image_to_switch.src = index_img
+    }, 500);
+
+    console.log(iteration_img1 % 4);
+    
+    switch (iteration_img1 % 4) {
+        case 1:
+            image_to_switch.style.animation = "translate-to-top 1s";
+            break;
+        case 2:
+            image_to_switch.style.animation = "translate-to-right 1s";
+            break;
+        case 3:
+            image_to_switch.style.animation = "translate-to-bottom 1s";
+            break;
+        case 0:
+            image_to_switch.style.animation = "translate-to-left 1s";
+            break;
+    }
+});
+
+btn_prev1.addEventListener("click", () => {
+    iteration_img1--;
+    // if the iteration is negative we set it to the last index of the array
+    if ( iteration_img1 < 0 )  {
+        iteration_img1 = image_project1.length - 1;
+    }
+    var index_img = image_project1[iteration_img1 % image_project1.length];
+
+    setTimeout(() => {
+        image_to_switch.src = index_img
+    }, 500);
+
+    console.log(iteration_img1 % image_project1.length);
+
+    switch (iteration_img1 % 4) {
+        case 1:
+            image_to_switch.style.animation = "translate-to-top 1s";
+            break;
+        case 2:
+            image_to_switch.style.animation = "translate-to-right 1s";
+            break;
+        case 3:
+            image_to_switch.style.animation = "translate-to-bottom 1s";
+            break;
+        case 0:
+            image_to_switch.style.animation = "translate-to-left 1s";
+            break;
+    }
+});
+
+
+const image_project2 = ["images/evangelion.jpg", "images/nets.jpg", "images/chat_gpt.jpg", "images/dall_e.jpg", "images/github_copilot.png"];
+
 
